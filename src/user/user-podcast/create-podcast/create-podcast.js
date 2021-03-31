ca5927c11692d267f1d967e0c471e1caf3c8334e
William Friefeldt <william@gmail.com>
2021-03-31 13:35:58 +0200

    Add podcast and show padcasts functionality

--- src/user/user-podcast/create-podcast/create-podcast.js
+++ src/user/user-podcast/create-podcast/create-podcast.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './create-podcast.css'
 import { VscLoading } from 'react-icons/vsc';
-
+import { auth, firestore, storage } from '../../../store/services/firebase';
 class CreatePodcast extends React.Component {
 
   constructor() {
@@ -12,16 +12,62 @@ class CreatePodcast extends React.Component {
         description: '',
         img: ''
       }, 
-      errors: []
+      errors: [],
+      loading: false
     }
     this.fileImgInput = React.createRef();
     this.setInput = this.setInput.bind(this);
+    this.checkFile = this.checkFile.bind(this);
+    this.uploadPodcast = this.uploadPodcast.bind(this);
   }
 
   setInput(event) {
     let podcastInfo = this.state.podcastInfo;
     podcastInfo[event.target.name] = event.target.value;
     this.setState({podcastInfo});
+  }
+
+  checkFile() {
+    if( this.fileImgInput.current.files.length > 0 ) {
+      let podcastInfo = this.state.podcastInfo;
+      podcastInfo['img'] = this.fileImgInput.current.files[0].name;
+      this.setState({podcastInfo});
+    }
+  }
+
+  checkInputs( info, img ) {
+    const errors = [];
+    if( info.name === '' ) errors.push( 'Podcasten måste ha ett namn' );
+    if( info.description === '' ) errors.push( 'Podcasten måste en beskrivning' );
+    if( img.current.files.length === 0 ) errors.push( 'Podcasten måste ha en omslagsbild' );
+    return errors;
+
+  }
+
+  async uploadPodcast() {
+    let podcastInfo = this.state.podcastInfo;
+    const errors = this.checkInputs( podcastInfo, this.fileImgInput );
+    this.setState({errors});
+    if( errors.length === 0 ) {
+      this.setState({loading:true});
+
+      const storageImgRef = storage.ref(podcastInfo.img);
+      const fileImgRes = await storageImgRef.put(this.fileImgInput.current.files[0]);
+      const uploadImgUrl = await fileImgRes.ref.getDownloadURL();
+
+      const userID = auth.currentUser.uid;
+      const userRef = firestore.doc(`companies/${userID}`);
+      const userData = await userRef.get();
+      let podcasts = userData.data()['podcasts'];
+      if( !podcasts ) {
+        podcasts = {};
+      } else {
+        podcasts[this.state.podcastInfo.name] = Object.assign({},podcastInfo);
+        podcasts[this.state.podcastInfo.name]['img'] = uploadImgUrl;
+        await userRef.set({ podcasts }, { merge:true });
+        this.setState({loading:false});
+      }
+    }
   }
 
   render() {
@@ -45,23 +91,28 @@ class CreatePodcast extends React.Component {
               </label>
               {this.state.podcastInfo.img === '' ?
                 <button><strong> + </strong> Lägg till bild </button>:
-                <p>{this.state.podcastInfo.img}</p>
+                <button><strong>{this.state.podcastInfo.img}</strong></button>
               }
-              <input className="upload-file-input" type="file" ref={this.fileImgInput} 
-                     onChange={() =>{ this.checkFile('img')}}/>
+              <input className="create-pod-file-input" type="file" ref={this.fileImgInput} 
+                     onChange={() =>{ this.checkFile()}}/>
 						</div>
           
-            <div className="create-pod-file-placeholder">
+            <div className="create-pod-text-placeholder">
               <label className="input-label">
                 Podcastens beskrivning
               </label>
               <textarea className="input-textarea" onChange={this.setInput} name="description" />
             </div>
+
+            <ul className="errors-list">
+              {this.state.errors.map( (item,i) => <li key={i}> {item} </li>)}
+            </ul>
+
         
             <div className="create-pod-upload">
 						  <button type="button" 
 								className={`${ this.state.errors.length > 0 ? 'error-btn' : '' }`}
-								onClick={ ()=>{}}>
+								onClick={ ()=>{this.uploadPodcast()}}>
 									{this.state.loading ?
 										<span className="loading"><VscLoading /></span> 
 									:
--- src/user/user-podcast/user-podcast.js
+++ src/user/user-podcast/user-podcast.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './user-podcast.css';
 import { VscLoading } from 'react-icons/vsc';
 import CreatePodcast from './create-podcast/create-podcast';
+import { auth, firestore } from '../../store/services/firebase';
 
 class UserPodcast extends React.Component {
 
@@ -16,8 +17,15 @@ class UserPodcast extends React.Component {
     this.openCreatePod = this.openCreatePod.bind(this);
   }
 
-  componentDidMount() {
-    setTimeout( ()=> this.setState({loading:false}), 1000);
+  async componentDidMount() {
+    this.setState({loading:false});
+    const userID = auth.currentUser.uid;
+    const userRef = firestore.doc(`companies/${userID}`);
+    const userData = await userRef.get();
+    let podcasts = userData.data()['podcasts'];
+    if(podcasts) {
+      this.setState({podcasts: Object.values( podcasts )});
+    }
   }
 
   openCreatePod() {
@@ -26,6 +34,18 @@ class UserPodcast extends React.Component {
   }
 
   render() {
+
+    const Podcasts = () => (
+      <div className="podcasts">
+        {this.state.podcasts.map( (podcast, index) => {
+          return <div className="podcast grid center-content">
+                    <img className="podcast-img" src={podcast.img} alt="podcast cover" />
+                    <h3>{podcast.name}</h3>
+                  </div>;
+        })}
+      </div>
+    );
+
     return(
       <div className="user-pod-container">
         <h2>Podcasts</h2>
@@ -33,7 +53,7 @@ class UserPodcast extends React.Component {
         {!this.state.loading ?
           <div className="user-pod-items">
             {this.state.podcasts.length !== 0 ? 
-              '' 
+              <Podcasts />
             :
               <div className="no-pod-container grid center-content">
                 <p> Det finns ingen podcast </p>
